7231bc96fa42321e22a239e73a358efb661f090b
Ronit Panda <ronit@superkit.io>
2023-10-28 17:01:40 +0530

    style: cleanup

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,16 +1,3 @@
-import { type NextURL } from "next/dist/server/web/next-url";
-import { env } from "~/env.mjs";
-
-export const checkCronRouteAuthEdge = (url: NextURL) => {
-  let isAuth = true;
-  url.searchParams.forEach((value, key) => {
-    if (key !== "key" || (key === "key" && value !== env.VERCEL_SECRET_KEY)) {
-      isAuth = false;
-    }
-  });
-  return isAuth;
-};
-
 export const isJson = (str: string) => {
   try {
     JSON.parse(str);
